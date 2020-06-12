c3cd4fb03a8cba0759f24b690f7950968ef1dffe
Brennan Romance <brennan.romance@gmail.com>
2020-06-12 17:25:45 -0400

    filter file upload on multer object

--- services/fileUpload.js
+++ services/fileUpload.js
@@ -3,15 +3,22 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const keys = require('../config/keys');
 
-aws.config.update({
+const s3 = new aws.S3({
   accessKeyId: keys.accessKeyID,
   secretAccessKey: keys.secretAccessKey,
   region: 'us-east-1'
-})
+});
 
-const s3 = new aws.S3();
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('File type must be either JPEG or PNG'), false);
+  }
+}
 
 const upload = multer({
+  fileFilter,
   storage: multerS3({
     s3: s3,
     bucket: 'oddsrprofilepics',
