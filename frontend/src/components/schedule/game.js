8cb63311b60246123330c924c2d9be0c8660bd25
Brennan Romance <brennan.romance@gmail.com>
2020-11-20 17:22:28 -0500

    add modal for choosing a pick to the game component

--- frontend/src/components/schedule/game.js
+++ frontend/src/components/schedule/game.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import { connect } from 'react-redux';
 import { parseDomain, fromUrl } from "parse-domain";
 import * as pickActions from '../../actions/game_actions';
 import './game.css'
 
-class SelectedPick extends React.Component {
+class SelectedGame extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      modalIsOpen: false
+    }
     this.chooseBet = this.chooseBet.bind(this)
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
   }
 
   chooseBet(e) {
@@ -22,6 +29,18 @@ class SelectedPick extends React.Component {
       gameOddId: selectedPick.GameOddId
     }
     this.props.createPick(payload)
+  }
+
+  openModal() {
+    this.setState({
+      modalIsOpen: true
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false
+    })
   }
 
   render() {
@@ -41,7 +60,7 @@ class SelectedPick extends React.Component {
             <p 
               className="selected-pick-bet-type"
               id={idx}
-              onClick={this.chooseBet}>Spread</p>
+              onClick={this.openModal}>Spread</p>
             <p>{this.props.game.HomeTeamName}: {book.HomePointSpread}</p>
             <p>{this.props.game.AwayTeamName}: {book.AwayPointSpread}</p>
           </div>
@@ -49,13 +68,24 @@ class SelectedPick extends React.Component {
             <p 
               className="selected-pick-bet-type"
               id={idx}
-              onClick={this.chooseBet}>Money Line</p>
+              onClick={this.openModal}>Money Line</p>
             <p>{this.props.game.HomeTeamName}: {book.HomeMoneyLine}</p>
             <p>{this.props.game.AwayTeamName}: {book.AwayMoneyLine}</p>
           </div>
         </div>
       )
     })
+
+    const customStyles = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+      }
+    };
 
     return (
       <div className="selected-pick-container">
@@ -64,6 +94,24 @@ class SelectedPick extends React.Component {
           <p> {this.props.game.AwayTeamName}</p>
         </div>
         {sitesArray}
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          // onAfterOpen={afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 /*ref={_subtitle => (subtitle = _subtitle)}*/>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
       </div>
     )
   }
@@ -78,4 +126,4 @@ const mapDispatchToProps = dispatch => ({
   createPick: (pick) => dispatch(pickActions.createPick(pick))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(SelectedPick);
+export default connect(mapStateToProps, mapDispatchToProps)(SelectedGame);
